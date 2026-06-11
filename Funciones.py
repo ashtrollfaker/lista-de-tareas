3aa7ea52473056f00212d38966908fc9b0f92b66
ashtrollfaker <151798750+ashtrollfaker@users.noreply.github.com>
2026-06-11 00:52:09 -0400

    Add files via upload

---
+++ Funciones.py
@@ -0,0 +1,12 @@
+print("---- Bienvenido/a a la Maquina de saludos ----")
+
+cantidad = int(input("Escoge cuantas veces quieres que te saluden "))
+def saludo():
+    print("Hola amigo del github")
+
+for i in range(cantidad):
+    if cantidad == 666:
+        print("Es el numero del diavlo mi loco")
+    else:
+        print(f"Saludo # {i+1}")
+        saludo()
